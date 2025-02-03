9eced4f3a29f2de262f7e72abbdcf619e7e85407
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2025-02-03 11:24:35 -0300

    new way to solve the challenge

--- python/second_maximum_number_in_a_list.py
+++ python/second_maximum_number_in_a_list.py
@@ -1,18 +1,23 @@
 '''Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given n scores. Store them in a list and find the score of the runner-up.
 '''
 
-n = int(input())
-arr = list(map(int, input().split()))
+# n = int(input())
+# arr = list(map(int, input().split()))
+numbers_list = [ -2,-3,-7,-5,-6, -7, -6 ]
+new_list = []
 
-max = max(arr)
-runner_up = -9999
-for number in arr:
-    if number < max and number > runner_up:
-        runner_up = number
-
-
-print(runner_up)
-
+# max = max(arr)
+# runner_up = -9999
+# for number in arr:
+#     if number < max and number > runner_up:
+#         runner_up = number
 
 
-    
+# print(runner_up)
+
+max_number = max(numbers_list)
+for n in numbers_list:
+    if n != max_number:
+        new_list.append(n)
+second_max_number = max(new_list)
+print(second_max_number)
